93a28547ffe5c99a505566d4cbcc9570421bacc5
ande202d <48085313+ande202d@users.noreply.github.com>
2020-06-01 10:31:37 +0200

    swagger

--- Skabeloner/Startup.cs
+++ Skabeloner/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace Rest
 {
@@ -35,6 +36,10 @@ namespace Rest
 
             //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             //services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Items API", Version = "v1.0" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,10 @@ namespace Rest
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0"));
+            app.UseSwaggerUI(c => c.RoutePrefix = "api/help");
 
             app.UseHttpsRedirection();
 
